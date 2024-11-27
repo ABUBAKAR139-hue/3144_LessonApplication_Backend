8d3d2e83679cfaa25577dec9cdb6d320a1f7430e
ABUBAKAR139-hue <abubakarummaabba2021@gmail.com>
2024-11-27 13:48:14 +0100

    MOD schema

--- models/Order.js
+++ models/Order.js
@@ -3,8 +3,11 @@ const mongoose = require("mongoose");
 const OrderSchema = new mongoose.Schema({
   name: { type: String, required: true },
   phone: { type: String, required: true },
-  lessonIDs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Lesson" }],
+  lessonIDs: [
+    { type: mongoose.Schema.Types.ObjectId, ref: "Lesson", required: true },
+  ],
   numberOfSpaces: { type: Number, required: true },
+  createdAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("Order", OrderSchema);
