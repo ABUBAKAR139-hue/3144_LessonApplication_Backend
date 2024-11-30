efd34b2413c4c15ba7c5b15ad2c52559982d9d09
ABUBAKAR139-hue <abubakarummaabba2021@gmail.com>
2024-11-30 09:17:02 +0100

    Append the origin url

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ const PORT = process.env.PORT || 5000;
 
 // CORS Configuration
 const corsOptions = {
-  origin: "https://abubakar139-hue.github.io/3144_LessonApplication_Frontend/",
+  origin: "https://abubakar139-hue.github.io",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
+  allowedHeaders: ["Content-Type"],
 };
 app.use(cors(corsOptions));
 
